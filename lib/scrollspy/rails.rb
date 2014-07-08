00bb97ddc1eefe31cb51dceeb7727e43227a481a
Kyle Szives <kjszives@gmail.com>
2014-07-07 21:42:56 -0400

    initial commit

---
+++ lib/scrollspy/rails.rb
@@ -0,0 +1,8 @@
+require "scrollspy/rails/version"
+
+module Scrollspy
+  module Rails
+    class Engine < ::Rails::Engine
+    end
+  end
+end
---
+++ lib/scrollspy/rails/version.rb
@@ -0,0 +1,5 @@
+module Scrollspy
+  module Rails
+    VERSION = "1.0.0"
+  end
+end
