bd2836cf1d5c026211ccf4717300fae9cca69f36
Kyle Szives <kjszives@gmail.com>
2014-07-07 21:58:58 -0400

    remove some files around

--- lib/scrollspy/rails.rb
+++ lib/scrollspy/rails.rb
@@ -3,6 +3,9 @@ require "scrollspy/rails/version"
 module Scrollspy
   module Rails
     class Engine < ::Rails::Engine
+      initializer 'static_assets.load_static_assets' do |app|
+        app.middleware.use ::ActionDispatch::Static, "#{root}/vendor"
+      end
     end
   end
 end
